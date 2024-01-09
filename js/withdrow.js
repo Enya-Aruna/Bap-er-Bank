223528a30c0d2eac7c55c6828bf82289b36066e0
enya <salmaislam489265@gmail.com>
2024-01-09 11:20:03 +0600

    withdraw

--- js/withdrow.js
+++ js/withdrow.js
@@ -1,20 +1,27 @@
+
+// Step --1 :
 document.getElementById('withdrow').addEventListener('click' , function(){
+    // step --2 :
     const WithdrawFeild = document.getElementById('withdrow-input') ;
     const newWithdrawAmountString = WithdrawFeild.value ; 
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+// step--3:
+    WithdrawFeild.value = ''
+// step--4:
     const previouswithdrawElement = document.getElementById('withdraw-total') ;
     const previouswithdrawAmmountString = previouswithdrawElement.innerText ;
     const previouswithdrawAmmount = parseFloat(previouswithdrawAmmountString);
     previouswithdrawElement.innerText =newWithdrawAmount  ; 
-   
-    WithdrawFeild.value = ''
-   if( currentWithdrawAmmount > previousBalanceTotal){
-    alert ('Ammount less')
-    
-   }
-   const currentWithdrawAmmount = previouswithdrawAmmount + newWithdrawAmount ;
-   previouswithdrawElement.innerText = currentWithdrawAmmount ;
+//  step--5:
+    const currentWithdrawAmmount = previouswithdrawAmmount + newWithdrawAmount ;
+    previouswithdrawElement.innerText = currentWithdrawAmmount ;
 
+//    if( currentWithdrawAmmount > previousBalanceTotal){
+//     alert ('Ammount less')
+    
+//    }
+
+// step--6:
     const balanceTotalElement = document.getElementById('balance-total') ;
     const previousBalanceTotalString = balanceTotalElement.innerText ;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString) ;
