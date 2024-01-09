bae0fb84b94ffeb15bf2261d0137287a5d1c8924
enya <salmaislam489265@gmail.com>
2024-01-09 12:12:37 +0600

    editing inner text

--- js/withdrow.js
+++ js/withdrow.js
@@ -5,28 +5,42 @@ document.getElementById('withdrow').addEventListener('click' , function(){
     const WithdrawFeild = document.getElementById('withdrow-input') ;
     const newWithdrawAmountString = WithdrawFeild.value ; 
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    if(isNaN(newWithdrawAmount)){
+        alert('please provide a number') ;
+        return ;
+    }
 // step--3:
     WithdrawFeild.value = ''
 // step--4:
     const previouswithdrawElement = document.getElementById('withdraw-total') ;
     const previouswithdrawAmmountString = previouswithdrawElement.innerText ;
     const previouswithdrawAmmount = parseFloat(previouswithdrawAmmountString);
-    previouswithdrawElement.innerText =newWithdrawAmount  ; 
-//  step--5:
-    const currentWithdrawAmmount = previouswithdrawAmmount + newWithdrawAmount ;
-    previouswithdrawElement.innerText = currentWithdrawAmmount ;
-
-//    if( currentWithdrawAmmount > previousBalanceTotal){
-//     alert ('Ammount less')
-    
-//    }
-
-// step--6:
+    // previouswithdrawElement.innerText =newWithdrawAmount  ; 
+    // step--6:
     const balanceTotalElement = document.getElementById('balance-total') ;
     const previousBalanceTotalString = balanceTotalElement.innerText ;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString) ;
-    const currentBalanceTotal = previousBalanceTotal - currentWithdrawAmmount ;
-    balanceTotalElement.innerText = currentBalanceTotal;
+    
+    if( newWithdrawAmount > previousBalanceTotal){
+      
+        alert ("Sorry! you don't have enough balance" )
+        return;
+       }
+       const currentWithdrawAmmount = previouswithdrawAmmount + newWithdrawAmount ;
+       previouswithdrawElement.innerText = currentWithdrawAmmount ;
+    
+           //step--7:
+           const currentBalanceTotal = previousBalanceTotal - currentWithdrawAmmount ;
+           balanceTotalElement.innerText = currentBalanceTotal;
+
+  
+
+
+
+
+
+  
+
 
  
 
